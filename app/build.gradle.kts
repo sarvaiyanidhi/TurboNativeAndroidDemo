ea4bd44da85f13ffe0cb2d0c2100e3ed9e3a9cb3
Nidhi Sarvaiya <sarvaiya.nidhi@gmail.com>
2024-04-03 23:38:53 -0400

    Add TurboNative Gradle dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("dev.hotwire:turbo:7.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
